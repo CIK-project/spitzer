3782516c81f75902358d90ae0d0ce450ef2e9781
yjh <yunjh1994@gmail.com>
2019-01-11 00:33:51 +0900

    cors test

--- hub/gql/server.go
+++ hub/gql/server.go
@@ -91,6 +91,13 @@ func (s *Server) Start() error {
 	s.schema = &schema
 
 	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+  	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if (*r).Method == "OPTIONS" {
+			return
+		}
+		
 		result := graphql.Do(graphql.Params{
 			Schema:        *s.schema,
 			RequestString: r.URL.Query().Get("query"),
